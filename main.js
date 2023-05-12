5ecb708d9226cc3f06065e06cb333bb627d3ae03
BorislavAntonic <95753945+BorislavAntonic@users.noreply.github.com>
2023-05-12 14:38:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-const kocka=document.getElementById("kocka");
+const box=document.getElementById("box");
 const timer=document.getElementById("timer");
+const game=document.getElementById("game");
+const totalScore=document.getElementById("score");
+const levelScore=document.getElementById("lvlScore");
+const target=document.getElementById("target");
+const level=document.getElementById("level")
 
 function getArray(start,next,end){
      let array=[];
@@ -16,15 +21,104 @@ function randomPosiition(){
     let x=getNumber(getArray(0,5,1350));
     let y=getNumber(getArray(0,5,450));
 
-    kocka.style.top=`${y}px`;
-    kocka.style.left=`${x}px`;
+    box.style.top=`${y}px`;
+    box.style.left=`${x}px`;
 
 
 }
-randomPosiition();
 function time(x){ 
     timer.innerHTML=`${x}`;
-     if(x>0){ setTimeout(() => { x--; time(x); }, 1000)}}
+     if(x>0){timeout= setTimeout(() => { x--; time(x); }, 1000)}
+     
+   
+     else{
+        if(parseInt(levelScore.innerHTML) >=parseInt( target.innerHTML)){
+            
+     levelPassed();
+     clearTimeout(timeout)
+     }
+     else{
+        levelFailed();
+   clearTimeout(timeout);
+     }
+    }
+}
+function startGame(){
+    document.getElementById("start").style.display="none"
+    game.style.display="flex"
+
+randomPosiition();
+time(30);
+}
+    
+
+function refreshScore(){
+    totalScore.innerHTML++;
+    levelScore.innerHTML++;
+}
+
+function clickOnBox(){
+    randomPosiition();
+    refreshScore();
+}
 
 
-     time(10)
+function levelPassed(){
+    game.style.display="none"
+    document.getElementById("passed").style.display="block"
+    document.getElementById("lvlpass").innerHTML=level.innerHTML;
+
+
+}
+
+function levelFailed(){
+    game.style.display="none"
+    document.getElementById("failed").style.display="block"
+    document.getElementById("lvlfail").innerHTML=level.innerHTML;
+
+}
+
+function level2(){
+    target.innerHTML=25;
+    levelScore.innerHTML=0;
+    level.innerHTML++;
+    time(30);
+
+    document.getElementById("passed").style.display="none";
+    game.style.display="flex"
+
+}
+
+function level3(){
+    target.innerHTML=30;
+    levelScore.innerHTML=0;
+    level.innerHTML++;
+     time(30);
+    document.getElementById("passed").style.display="none";
+    game.style.display="flex"
+    
+}
+function gameOver(){
+    document.getElementById("passed").style.display="none";
+    document.getElementById("game-over").style.display="block";
+    document.getElementById("endScore").innerHTML=totalScore.innerHTML;
+
+
+}
+
+function nextLevel(){
+    if(level.innerHTML=="1"){
+        level2();
+    }
+    else if(level.innerHTML=="2"){
+        level3();
+    }
+    else{
+        gameOver();
+    } 
+  }
+
+function playAgain(){
+    window.location.reload();
+  
+  }
